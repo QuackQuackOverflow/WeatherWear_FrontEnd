19af5ccf6071d044ac608b58b84b0b93a649b44e
taeseok <c0dingduck1in9@gmail.com>
2024-10-06 16:44:11 +0900

    first status of project WeatherWear

---
+++ app/src/main/java/com/example/weatherwear/LoginActivity.kt
@@ -0,0 +1,31 @@
+package com.example.weatherwear
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // 계정 생성 버튼 클릭 시 RegisterActivity로 전환
+        val createAccountButton = findViewById<Button>(R.id.createAccountButton) // 계정 생성 버튼 ID
+        createAccountButton.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        // '메인화면 바로 가기' 버튼 클릭 시 MainActivity로 전환
+        val moveToMainButton = findViewById<Button>(R.id.moveToMainButton) // 메인화면 바로가기 버튼 ID
+        moveToMainButton.setOnClickListener {
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/weatherwear/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.weatherwear
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // 2초 동안 splash 화면을 띄운 후 LoginActivity로 전환
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish() // SplashActivity를 종료
+        }, 2000) // 2000ms = 2초
+    }
+}
