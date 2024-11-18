7cc58f2576ad81a0e6518ddb27ff5cc6d84353e1
taeseok <c0dingduck1in9@gmail.com>
2024-11-19 01:10:22 +0900

    deleted uncertain route of Back-End API

--- app/src/main/java/com/example/weatherwear/data/api/ApiService.kt
+++ app/src/main/java/com/example/weatherwear/data/api/ApiService.kt
@@ -34,25 +34,25 @@ interface ApiService {
      * Region 관련 API
      */
     // 1. Region 객체를 보내고 지역 이름, 날씨 정보, 의상 세트를 포함한 RWCResponse를 받는 API
-    @POST("api/regions/details")
+    @POST("/")
     suspend fun getRegionDetails(@Body region: Region): Response<RWCResponse>
 
     // 2. Region 객체를 보내고 지역 이름만 반환받는 API
-    @POST("api/regions/name")
+    @POST("/")
     suspend fun getRegionName(@Body region: Region): Response<Region>
 
     // 3. Region 객체를 보내고 날씨 정보만 반환받는 API
-    @POST("api/regions/weather")
+    @POST("/")
     suspend fun getWeather(@Body region: Region): Response<Weather>
 
     // 4. Region 객체를 보내고 의상 세트만 반환받는 API
-    @POST("api/regions/clothing-set")
+    @POST("/")
     suspend fun getClothingSet(@Body region: Region): Response<ClothingSet>
 
     /**
      * Review 관련 API
      */
     // 리뷰 데이터를 서버에 전송
-    @POST("api/reviews")
+    @POST("/")
     fun submitReviews(@Body reviews: List<Review>): Call<Void>
 }
