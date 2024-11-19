b32fdbd7c9838878acace12f7f6a9ae780217328
taeseok <c0dingduck1in9@gmail.com>
2024-11-19 12:51:31 +0900

    renamed buttons to test API communication

--- app/src/main/java/com/example/weatherwear/data/model/Review.kt
+++ app/src/main/java/com/example/weatherwear/data/model/Review.kt
@@ -1,6 +1,6 @@
 package com.example.weatherwear.data.model
 
 data class Review(
-    val clothingId: String, // 평가받은 옷 ID 또는 이름
+    val clothingId: String, // 평가받은 set의 ID(식별자)
     val feedback: String    // "추웠어요", "더웠어요", "마음에 들어요"
 )
